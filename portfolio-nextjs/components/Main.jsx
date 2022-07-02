fd556f4f3dbf81e3a94611d962f01141345fe6f9
laujoseph <97426227+laujoseph@users.noreply.github.com>
2022-07-02 11:14:41 +0800

    Created main page

--- portfolio-nextjs/components/Main.jsx
+++ portfolio-nextjs/components/Main.jsx
@@ -1,13 +1,45 @@
 import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill, BsFolder } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener, noreferrer");
+  };
   return (
     <div className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <p className="uppercase text-sm tracking-widest">
-            Let's build something together
+          <h1 className="py-4 text-gray-700">
+            Hi, I'm <span className="text-[#d67f0e]">Joseph</span>
+          </h1>
+          <h1 className="py-2 text-gray-700">A Software Engineer</h1>
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+            With a passion for problem solving and love of learning, I believe
+            in developing creative solutions to meet user needs. I also enjoy
+            working in teams and value communication.
           </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn
+                onClick={() =>
+                  openInNewTab("https://www.linkedin.com/in/joseph-lau-ys/")
+                }
+              />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub
+                onClick={() => openInNewTab("https://github.com/laujoseph")}
+              />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
         </div>
       </div>
     </div>
